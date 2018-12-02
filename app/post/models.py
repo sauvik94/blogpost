6d3080a0514230a638cdee8aef5955f18dbc08b8
Sauvik BIswas <Sauvik>
2018-12-02 18:10:03 +0530

    Object name in Django admin

--- app/post/models.py
+++ app/post/models.py
@@ -7,3 +7,6 @@ class Post(models.Model):
     story = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     is_deleted = models.BooleanField(null=False, default=False)
+
+    def __str__(self):
+        return 'Title : ' + self.title
